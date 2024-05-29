bf2dd5594c5454291c92f7df9c35ebb123360318
quanzip <quanaqtzip4@gmail.com>
2024-05-29 11:51:50 +0700

    change api

--- backend/finder_service/src/main/java/com/viettel/ontap_thay_cuong/controller/SiteController.java
+++ backend/finder_service/src/main/java/com/viettel/ontap_thay_cuong/controller/SiteController.java
@@ -4,8 +4,6 @@ import com.viettel.ontap_thay_cuong.entities.SiteEntity;
 import com.viettel.ontap_thay_cuong.service.SiteService;
 import com.viettel.ontap_thay_cuong.service.dto.SiteDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,18 +18,21 @@ public class SiteController {
     @GetMapping("/sites")
     public List<SiteEntity> getSites(@RequestParam(value = "siteName", required = false) String siteName) {
         if (siteName == null || siteName.isEmpty()) {
-            return this.siteService.findAllSiteByStatus((short)1);
+            return this.siteService.findAllSiteByStatus((short) 1);
         }
-        return siteService.getSites(siteName);
+        return siteService.getSites("%" + siteName + "%");
     }
+
     @GetMapping("/sites/{id}")
     public SiteEntity getSiteById(@PathVariable("id") String id) {
         return siteService.getSiteById(id);
     }
+
     @PostMapping("/sites/create")
     public SiteEntity createSite(@RequestBody SiteDTO siteDTO) {
         return siteService.createSite(siteDTO);
     }
+
     @PostMapping("/sites/update")
     public SiteEntity updateSite(@RequestBody SiteDTO siteDTO) {
         return siteService.updateSite(siteDTO);
